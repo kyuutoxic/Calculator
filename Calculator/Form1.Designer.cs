fd3cc06931d0caf8ad9ba051760c178865308c1e
kyuutoxic <lam.nv1403@gmail.com>
2022-03-18 23:33:06 +0700

    Kiem tra dieu kien truoc khi cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -97,7 +97,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(205, 204);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 25);
+            this.button1.Size = new System.Drawing.Size(83, 25);
             this.button1.TabIndex = 4;
             this.button1.Text = "Cộng";
             this.button1.UseVisualStyleBackColor = true;
